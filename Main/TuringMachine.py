9fc19c80ce873bc41c0a98d95c6ccbaa2853fa43
Pedro Moises Marin Angarita <pedromoisesmarin0566@gmail.com>
2026-05-15 22:17:01 -0500

    Defined how Wordle_Turing_Machine() would work

--- Main/TuringMachine.py
+++ Main/TuringMachine.py
@@ -6,40 +6,53 @@ def Wordle_Turing_Machine(wordle_input):
     """
     La máquina de Turing siempre cambiará según la palabra del Wordle,
     por ende, no es viable usar una máquina de Turing ya predefinida,
-    sino una que se adapte según la palabra del Wordle.
+    sino una que se adapte según la palabra del Wordle. Debido a esto,
+    es necesario definir los alfabetos y transiciones previo a la creación
+    de la máquina de Turing.
     """
 
-    # Convertimos el wordle_input en un array donde cada elemento representará cada símbolo.
-    wordle_input = wordle_input.lower()
-    wordle_array = list(wordle_input)
+    if len(wordle_input)==5:
+        # Convertimos el wordle_input en un array donde cada elemento representará cada símbolo.
+        wordle_input = wordle_input.lower()
+        wordle_array = list(wordle_input)
 
-    #La maquina de Turing no puede determinar que símbolos contiene wordle_array, deben especificarse, previo a su implementación
-    wordle_alphabet = set('abcdefghijklmnñopqrstuvwxyz')
-    wordle_alphabet |= set(wordle_array)
+        # Creamos los alfabetos
+        wordle_alphabet = set('abcdefghijklmnñopqrstuvwxyz')
+        wordle_alphabet |= set(wordle_array)
 
-    tape_alphabet = set('012-#')
-    tape_alphabet |= set(wordle_alphabet.copy())
+        tape_alphabet = set('012-#')
+        tape_alphabet |= set(wordle_alphabet.copy())
 
-    not_wordle_alphabet = wordle_alphabet - set(wordle_array)
+        alphabet_wout_array = wordle_alphabet - set(wordle_array)
 
-    """
-    Revisar como realizar las transiciones mediante uso de fors 
-    """
+        # Los for's nos permitirán definir las transiciones sin importar cuál sea la palabra
+        chain_length = len(wordle_array)
 
-    wordle_tm=MNTM(
-        states={'q0','q1','q2','q3','q4','q5','q6','q7','q8','q9','q10','q11'},
-        input_symbols=wordle_alphabet,
-        tape_symbols=tape_alphabet,
-        n_tapes=3,
-        transitions={
+        for i in range(0, chain_length):
+            current_state = f'q{i}'
+            next_state = f'q{i + 1}'
 
-        },
-        initial_state='q0',
-        blank_symbol='#',
-        final_states={'---'}
-    )
+        wordle_tm = MNTM(
+            states={'q0', 'q1', 'q2', 'q3', 'q4', 'q5', 'q6', 'q7', 'q8', 'q9', 'q10', 'q11'},
+            input_symbols=wordle_alphabet,
+            tape_symbols=tape_alphabet,
+            n_tapes=2,
+            transitions={
+                'q0': {
+                    (wordle_array[0], '#'): [('q0'), (('a', 'R'), ('#', 'R'))],
+                },
+            },
+            initial_state='q0',
+            blank_symbol='#',
+            final_states={'---'}
+        )
 
-    return wordle_tm
+        return wordle_tm
+
+    else:
+        print("Error idk")
+        return None
+
 
 # Creamos una función encargada de llamar Wordle_Turing_Machine() con nuestra palabra a descifrar
 
